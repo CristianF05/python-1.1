3f0fb8130418301d4b44767c1d3a9253ada7df93
Iamfrans <1323544@senati.pe>
2023-02-23 11:35:36 -0500

    desarrollo de la multiplicación

--- suma y multiplicación con la misma variable/suma y multiplicacion con variables.py
+++ suma y multiplicación con la misma variable/suma y multiplicacion con variables.py
@@ -4,3 +4,6 @@ n = 5
 #suma con las variables y imprimer el resultado
 suma = m + n
 print ("el resultado de la suma es: " + str(suma) )
+#multiplicacion con las variables y imprimir el resultado
+mul = m * n
+print ("el resultado de la multiplicacion es: " + str(mul))
