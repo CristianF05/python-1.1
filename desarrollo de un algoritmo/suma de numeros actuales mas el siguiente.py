6bafbaaf4726d45c6eefc8ac7b1f3b388c4ff4ba
Iamfrans <1323544@senati.pe>
2023-02-24 08:21:06 -0500

    cracion de las variables para el uso de un ciclo for

---
+++ desarrollo de un algoritmo/suma de numeros actuales mas el siguiente.py
@@ -0,0 +1,5 @@
+#suma del numero actual y su anterior
+
+#cracion de las variables para el uso de un ciclo for
+n = int(input("Ingrese el valor final: "))
+m = 0
--- hello word.py
+++
@@ -1,5 +0,0 @@
-m = int(input("ponga el primer valor: "))
-n = int(input("ponga el segundo valor: "))
-suma = m + n
-input("resultado: " + str(suma))
-
