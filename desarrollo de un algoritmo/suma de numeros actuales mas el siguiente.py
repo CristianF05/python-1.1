97ae47265ecbbcfcd95df4c42077f0164caaccd8
Iamfrans <1323544@senati.pe>
2023-02-24 10:14:26 -0500

    ajustes del ciclo for para agregar la suma del 0

--- desarrollo de un algoritmo/suma de numeros actuales mas el siguiente.py
+++ desarrollo de un algoritmo/suma de numeros actuales mas el siguiente.py
@@ -3,7 +3,6 @@
 #cracion de las variables para el uso de un ciclo for
 n = int(input("numero final:"))
 #creacion del ciclo for
+print("0 + 0 = 0")
 for i in range(1,n+1):
-    print(i,"+",i-1,"=",i+(i-1))
-    
-    
+    print(i,"+",i-1,"=",i+(i-1))   
