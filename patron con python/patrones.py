75655b10549d4fa37cf9ad9a4008f840fa47e6c2
CritianF05 <1482670@senati.pe>
2023-02-25 09:45:17 -0500

    uso del bucle for

--- patron con python/patrones.py
+++ patron con python/patrones.py
@@ -1,3 +1,8 @@
 #creacion de un patron de numeros con python
 #creacion de las variables
 m = int(input("ponga el asta el numeor que se quiere ejecutar: "))
+n = 0
+#uso de el bucle for
+for d in range(m+1):
+    print(str(d) * d)
+    
